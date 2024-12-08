2f16ef0bbc942d11b236db4eec23efda5188e7c4
FreakMediaLP <felix@wahlfeld.de>
2024-12-08 23:48:38 +0100

    - added eula and alias logic

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ function get_server_name {
         if [ ! -d "$SERVER_NAME" ]; then
             break
         else
-            echo "Server \"$SERVER_NAME\" already exists, chose a different name"
+            echo "Server \"$SERVER_NAME\" already exists, choose a different name"
         fi
     done
 }
@@ -64,44 +64,79 @@ function download_paper_jar {
     local jar_name="paper-$MINECRAFT_VERSION-$latest_build.jar"
     local download_url="https://api.papermc.io/v2/projects/paper/versions/$MINECRAFT_VERSION/builds/$latest_build/downloads/$jar_name"
 
-    # Save user directory
-    local user_home=$(eval echo ~$USER)
-    # Absolute path without user directory
-    local relative_path=${PWD/#$user_home/}
+    local relative_path=${PWD/#$HOME/}
     mkdir -p "$SERVER_NAME"
 
-    echo "Created directory \"$relative_path/$SERVER_NAME\""
-
+    echo "Created directory \"~$relative_path/$SERVER_NAME\""
     echo
     echo "Downloading latest $build_type build for Minecraft version \"$MINECRAFT_VERSION\"..."
     local download_response=$(curl -s -w "%{http_code}" -o "$SERVER_NAME/paper.jar" "$download_url")
-
     if [ "$download_response" -ne 200 ]; then
         echo "Failed to download paper.jar from \"$download_url\" status_code: \"$download_response\""
         exit 1
     else
         echo "Successfully downloaded paper.jar into server directory"
-        accept_eula
+        initial_setup=true
     fi
 }
 
 # Function to accept EULA
 function accept_eula {
-    read -p "Accept the EULA (https://aka.ms/MinecraftEULA) [Y/n]: " accept
-    accept=${accept:-Y}  
+    EULA_URL="https://raw.githubusercontent.com/FreakMediaLP/PaperServerSetup/main/eula.txt"
+    echo
+    read -p "Accept the EULA (https://aka.ms/MinecraftEULA)? [Y/n]: " accept
+    accept=${accept:-Y}
     if [[ "$accept" =~ ^[Yy]$ ]]; then
-        echo "eula=true" > "$SERVER_NAME/eula.txt"
-        echo "EULA accepted, created eula.txt"
+        curl -fsSL "$EULA_URL" -o "$SERVER_NAME/eula.txt"
+        echo "EULA accepted, downloaded eula.txt"
     else
         echo "eula=false" > "$SERVER_NAME/eula.txt"
-        echo "EULA not accepted, exiting."
-        exit 1
+        echo "EULA not accepted."
     fi
 }
 
-# Main script logic
+# Function to create aliases
+function create_aliases {
+    echo
+    read -p "Create Tmux-Aliases for easier Server-Operation? [Y/n]: " create_aliases_response
+    create_aliases_response=${create_aliases_response:-Y}
+
+    if [[ "$create_aliases_response" =~ ^[Yy]$ ]]; then
+        local relative_path=${PWD/#$HOME/}
+
+        {
+            echo "# $SERVER_NAME"
+            echo "alias ${SERVER_NAME}_console='tmux attach -t $SERVER_NAME'"
+            echo "alias ${SERVER_NAME}_start='tmux new-session -d -s $SERVER_NAME \"cd ~$relative_path/$SERVER_NAME && java -Xms2G -Xmx6G -jar paper.jar\"'"
+            echo "alias ${SERVER_NAME}_kill='tmux send-keys -t $SERVER_NAME \"stop\" Enter'"
+            echo "alias ${SERVER_NAME}_restart='${SERVER_NAME}_kill && while tmux has-session -t $SERVER_NAME 2>/dev/null; do sleep 1; done && ${SERVER_NAME}_start'"
+            echo
+        } > ~/.bashrc.tmp
+        cat ~/.bashrc >> ~/.bashrc.tmp
+        mv ~/.bashrc.tmp ~/.bashrc
+
+        source ~/.bashrc  # Apply aliases
+
+        echo "Added following Aliases (make sure to install tmux)"
+        echo
+        echo "${SERVER_NAME}_console:   opens server console (tmux session)"
+        echo "${SERVER_NAME}_start:     starts server (with provided RAM limits)"
+        echo "${SERVER_NAME}_kill:      stops server (kill only used for easier naming)"
+        echo "${SERVER_NAME}_restart:   stops & starts server"
+        echo
+    else
+        echo "No Aliases created"
+    fi
+}
+
+# Initial setup logic
+initial_setup=false
 get_server_name
 get_minecraft_version
-
-# Download the Paper JAR
 download_paper_jar
+
+
+if [ "$initial_setup" = true ]; then
+    accept_eula
+    create_aliases
+fi
